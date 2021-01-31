24c72bbb3c6f20e4fff2e29b7d802623e2ed57fd
Josh Mann <jlmann72@gmail.com>
2021-01-31 08:42:33 -0500

    Shuffling algorithm and player creation Created and tested shuffle algorithm. Tested dealer and human player creation.

--- Program.cs
+++ Program.cs
@@ -67,7 +67,79 @@ namespace BlackJackCS2
             // Prompt player for name
             Console.WriteLine("Welcome. What's your name?");
             string userName = Console.ReadLine();
-            Console.WriteLine($"Hi {userName}");
+
+            // Test userName population
+            // Console.WriteLine($"Hi {userName}");
+
+            // Create Deck
+            var deck = new List<Card>();
+            // Define arrays of suits and faces for assignment
+            var assignedSuit = new string[] { "\u2660", "\u2663", "\u2665", "\u2666" };
+            var assignedFace = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+            // Assign suits and faces to cards
+            // Suit Loop
+            foreach (var suit in assignedSuit)
+            {
+                // Face Loop
+                foreach (var face in assignedFace)
+                {
+                    // create new card instance
+                    var newCard = new Card();
+                    {
+                        // Assign suit and face to card
+                        newCard.Suit = suit;
+                        newCard.Face = face;
+                    }
+
+                    // Add new card to deck
+                    deck.Add(newCard);
+                }
+            }
+
+            // Test deck creation
+            // foreach (var createdCard in deck)
+            // {
+            //     Console.WriteLine($"{createdCard.Face}{createdCard.Suit}");
+            // }
+
+            // Shuffling Algorithm
+
+            // Declare Variables
+            var numberOfCards = deck.Count();
+            var randomNumberGenerator = new Random();
+
+            // Shuffle Loop
+            for (var rightIndex = numberOfCards - 1; rightIndex > 0; rightIndex--)
+            {
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                var leftCard = deck[rightIndex];
+                var rightCard = deck[leftIndex];
+                deck[rightIndex] = rightCard;
+                deck[leftIndex] = leftCard;
+            }
+
+            // Test deck creation
+            // foreach (var createdCard in deck)
+            // {
+            //     Console.WriteLine($"{createdCard.Face}{createdCard.Suit}");
+            // }
+
+            // Create Dealer player instance
+            var dealer = new Player();
+            {
+                dealer.PlayerName = "Dealer";
+            }
+
+            Console.WriteLine($"{dealer.PlayerName}");
+
+            // Create human player instance
+            var humanPlayer = new Player();
+            {
+                humanPlayer.PlayerName = userName;
+            }
+
+            Console.WriteLine($"{humanPlayer.PlayerName}");
         }
     }
 }
